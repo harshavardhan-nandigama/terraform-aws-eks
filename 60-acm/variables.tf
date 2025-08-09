cf53029dc85e9f220b1fff0e483f7677c21c893a
unknown <harha@gmail.com>
2025-08-09 17:58:26 +0530

    k8

---
+++ 60-acm/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z08522663KW6H1TJ65HHE"
+}
+
+variable "zone_name" {
+    default = "harshavardhan.site"
+}
